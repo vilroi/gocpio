7eba5a333e2e035589e4cdfd562800bd777bb06a
vilr0i <vilr0i@protonmail.com>
2024-01-18 00:39:42 -0800

    Created new struct RawCpioStruct to differentiate between the raw representation of a cpio header and the representation that work is actually done on.

--- cpio.go
+++ cpio.go
@@ -4,13 +4,42 @@ import (
 	"encoding/binary"
 	"fmt"
 	"os"
+	"syscall"
+
+	unsafe "unsafe"
 )
 
 const (
 	magicval string = "07070100"
 )
 
+type CpioMember struct {
+	header CpioHeader
+	data   []byte
+}
+
 type CpioHeader struct {
+	Magic     string
+	Inode     uint64
+	Mode      uint64
+	Uid       uint64
+	Gid       uint64
+	Nlink     uint64
+	Mtime     uint64
+	FileSize  uint64
+	DevMajor  uint64
+	DevMinor  uint64
+	RDevMajor uint64
+	RDevMinor uint64
+	NameSize  uint64
+	Check     uint64
+}
+
+func (header CpioHeader) VerifyMagic() bool {
+	return header.Magic == magicval
+}
+
+type RawCpioHeader struct {
 	Magic     [8]byte
 	Inode     [8]byte
 	Mode      [8]byte
@@ -27,16 +56,33 @@ type CpioHeader struct {
 	Check     [8]byte
 }
 
-func (header CpioHeader) Dump() {
-	fmt.Printf("Magic: %s\n", string(header.Magic[:]))
-	fmt.Printf("Inode: %s\n", string(header.Inode[:]))
-	fmt.Printf("Mode: %s\n", string(header.Mode[:]))
-	fmt.Printf("Uid: %s\n", string(header.Uid[:]))
-	fmt.Printf("NameSize: %s\n", string(header.NameSize[:]))
+func (rawheader RawCpioHeader) ToCpioHeader() CpioHeader {
+	var cpioheader CpioHeader
+
+	cpioheader.Magic = string(rawheader.Magic[:])
+	cpioheader.Inode = binary.LittleEndian.Uint64(rawheader.Inode[:])
+	cpioheader.Mode = binary.LittleEndian.Uint64(rawheader.Mode[:])
+	cpioheader.Uid = binary.LittleEndian.Uint64(rawheader.Uid[:])
+	cpioheader.Gid = binary.LittleEndian.Uint64(rawheader.Gid[:])
+	cpioheader.Nlink = binary.LittleEndian.Uint64(rawheader.Nlink[:])
+	cpioheader.Mtime = binary.LittleEndian.Uint64(rawheader.Mtime[:])
+	cpioheader.FileSize = binary.LittleEndian.Uint64(rawheader.FileSize[:])
+	cpioheader.DevMajor = binary.LittleEndian.Uint64(rawheader.DevMajor[:])
+	cpioheader.DevMinor = binary.LittleEndian.Uint64(rawheader.DevMinor[:])
+	cpioheader.RDevMajor = binary.LittleEndian.Uint64(rawheader.RDevMajor[:])
+	cpioheader.RDevMinor = binary.LittleEndian.Uint64(rawheader.RDevMinor[:])
+	cpioheader.NameSize = binary.LittleEndian.Uint64(rawheader.NameSize[:])
+	cpioheader.Check = binary.LittleEndian.Uint64(rawheader.Check[:])
+
+	return cpioheader
 }
 
-func (header CpioHeader) VerifyMagic() bool {
-	return string(header.Magic[:]) == magicval
+func (rawheader RawCpioHeader) Dump() {
+	fmt.Printf("Magic: %s\n", string(rawheader.Magic[:]))
+	fmt.Printf("Inode: %s\n", string(rawheader.Inode[:]))
+	fmt.Printf("Mode: %s\n", string(rawheader.Mode[:]))
+	fmt.Printf("Uid: %s\n", string(rawheader.Uid[:]))
+	fmt.Printf("NameSize: %s\n", string(rawheader.NameSize[:]))
 }
 
 func main() {
@@ -48,12 +94,47 @@ func main() {
 	fd, err := os.Open(os.Args[1])
 	check(err)
 
-	var header CpioHeader
-	binary.Read(fd, binary.LittleEndian, &header)
+	info, err := fd.Stat()
+	check(err)
 
-	if header.VerifyMagic() == false {
-		fmt.Println("not valid cpio header")
+	for nread := 0; nread < int(info.Size()); {
+		var raw_header RawCpioHeader
+		err = binary.Read(fd, binary.LittleEndian, &raw_header)
+		check(err)
+		nread += int(unsafe.Sizeof(raw_header))
+
+		header := raw_header.ToCpioHeader()
+		if !header.VerifyMagic() {
+			fmt.Fprintf(os.Stderr, "invalid magc number: %s\n", string(raw_header.Magic[:]))
+			os.Exit(1)
+		}
+
+		//	namebuf := make([]byte, raw_header.NameSize
+		break
 	}
+
+	/*
+		var header CpioHeader
+		binary.Read(fd, binary.LittleEndian, &header)
+
+		if header.VerifyMagic() == false {
+			fmt.Println("not valid cpio header")
+		}
+	*/
+}
+
+func mmap(path string) []byte {
+	fd, err := syscall.Open(os.Args[1], syscall.O_RDONLY, 0)
+	check(err)
+
+	var statbuf syscall.Stat_t
+	err = syscall.Fstat(fd, &statbuf)
+	check(err)
+
+	bytes, err := syscall.Mmap(fd, 0, int(statbuf.Size), syscall.PROT_READ, syscall.MAP_ANONYMOUS|syscall.MAP_PRIVATE)
+	check(err)
+
+	return bytes
 }
 
 func check(err error) {
